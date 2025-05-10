c8a987fa76a1781e1acecca64673cdba20cf03b9
dillonallen92 <dillon.allen.92@gmail.com>
2025-05-10 10:39:12 -0700

    wrote some basic python code to refamiliarize myself with the syntax

---
+++ basic_python_review/python_review.py
@@ -0,0 +1,15 @@
+# basic printing
+print("Hello, World!")
+name = "dillon"
+print(f"Hello, {name}!")
+
+# List comprehension
+squares = [x**2 for x in range(1,11)]
+print(squares)
+
+# functions
+def square_function(val):
+  return val*val
+
+print(f"The square of 4 is {square_function(4)}")
+
