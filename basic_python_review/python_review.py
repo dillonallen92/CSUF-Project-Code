362d8c6dbe443c8ed74d0c5c6c5c9ff56e4e97fd
dillonallen92 <dillon.allen.92@gmail.com>
2025-05-10 10:49:11 -0700

    added some basic functionality with packages like numpy, seaborn, and matplotlib

--- basic_python_review/python_review.py
+++ basic_python_review/python_review.py
@@ -13,3 +13,27 @@ def square_function(val):
 
 print(f"The square of 4 is {square_function(4)}")
 
+# Library specific code
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+sns.set_theme()
+# x = np.linspace(-10, 10, 1000)
+# y = np.sin(x)
+# plt.plot(x,y)
+# plt.show()
+
+# seaborn tutorial
+# Load an example dataset
+tips = sns.load_dataset("tips")
+
+# Create a visualization
+sns.relplot(
+    data=tips,
+    x="total_bill", y="tip", col="time",
+    hue="smoker", style="smoker", size="size",
+)
+plt.show()
+
+
