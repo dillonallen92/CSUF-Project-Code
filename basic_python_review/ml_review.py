af0350c4be626d135a640e8edf0dbce8ef81e427
dillonallen92 <dillon.allen.92@gmail.com>
2025-06-25 11:12:49 -0700

    added the LSTM architecture

--- basic_python_review/ml_review.py
+++ basic_python_review/ml_review.py
@@ -36,8 +36,20 @@ X_train, y_train = create_dataset(y[:train_idx], lookback=50)
 X_test, y_test   = create_dataset(y[train_idx:], lookback = 50)
 print(f"Train Shape: X: {X_train.shape}, y: {y_train.shape}\nTest Shape: X: {X_test.shape}, y: {y_test.shape}")
 
+# Create SineAnalyzer class that has the LSTM architecture
 class SineAnalyzer(nn.Module):
   
   def __init__(self, input_size, hidden_size, num_layers):
-    pass
+    super().__init__()
+    self.lstm = nn.LSTM(input_size= input_size,
+                        hidden_size= hidden_size,
+                        num_layers= num_layers,
+                        batch_first= True)
+    self.linear = nn.Linear(hidden_size, input_size)
+  
+  def forward(self, x):
+    out, _ = self.lstm(x)
+    out    = self.linear(out)
+    return out 
+
 
