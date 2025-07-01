9a399f876c4b670d971c2a3c210e14a02839fa0a
dillonallen92 <dillon.allen.92@gmail.com>
2025-07-01 15:33:46 -0700

    added dropout parameter to LSTM

--- Project/code/models.py
+++ Project/code/models.py
@@ -1,9 +1,9 @@
 import torch.nn as nn
 
 class LSTM(nn.Module):
-  def __init__(self, input_size, hidden_size, num_layers = 1):
+  def __init__(self, input_size, hidden_size, dropout, num_layers = 1):
     super().__init__()
-    self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first = True)
+    self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first = True, dropout=dropout)
     self.linear = nn.Linear(hidden_size, 1) # single output (predicting VF case rate)
   
   def forward(self, x):
