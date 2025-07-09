69b02ec6e385e1c08227a247b364e20e6b96c98a
dillonallen92 <dillon.allen.92@gmail.com>
2025-07-09 12:21:00 -0700

    finished drafting the transformer model from scratch, need to test it from the medium article. Then I can work on the actual project

--- deep_learning_models/Online Tutorials/Transformer Tutorials/tf_from_scratch.py
+++ deep_learning_models/Online Tutorials/Transformer Tutorials/tf_from_scratch.py
@@ -92,9 +92,61 @@ class EncoderLayer(nn.Module):
     x                  = self.norm2(x + self.dropout(feedforward_output))
     return x
 
+class DecoderLayer(nn.Module):
+  def __init__(self, d_model, num_heads, d_ff, dropout):
+    super().__init__()
+    self.self_attention  = MultiHeadAttention(d_model, num_heads)
+    self.cross_attention = MultiHeadAttention(d_model, num_heads)
+    self.feedforward     = PositionWiseFeedForward(d_model, d_ff)
+    self.norm1           = nn.LayerNorm(d_model)
+    self.norm2           = nn.LayerNorm(d_model)
+    self.norm3           = nn.LayerNorm(d_model)
+    self.dropout         = nn.Dropout(dropout)
+  
+  def forward(self, x, enc_output, src_mask, tgt_mask):
+    attention_output   = self.self_attention(x, x, x, tgt_mask)
+    x                  = self.norm1(x + self.dropout(attention_output))
+    attention_output   = self.cross_attention(x, enc_output, enc_output, src_mask)
+    x                  = self.norm2(x + self.dropout(attention_output))
+    feedforward_output = self.feedforward(x)
+    x                  = self.norm3(x + self.dropout(feedforward_output))
 
+# Finally, we can make the transformer model
+class Transformer(nn.Module):
+  def __init__(self, src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout):
+    super().__init__()
+    self.encoder_embedding   = nn.Embedding(src_vocab_size, d_model)
+    self.decoder_embedding   = nn.Embedding(tgt_vocab_size, d_model)
+    self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
 
+    self.encoding_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+    self.decoding_layers = nn.ModuleList([DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
 
+    self.fully_connected_layer = nn.Linear(d_model, tgt_vocab_size)
+    self.dropout = nn.Dropout(dropout)
 
+  def generate_mask(self, src, tgt):
+    src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
+    tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3)
+    seq_length = tgt.size(1)
+    nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)).bool()
+    tgt_mask = tgt_mask & nopeak_mask
+    return src_mask, tgt_mask
 
+  def forward(self, src, tgt):
+    src_mask, tgt_mask = self.generate_mask(src, tgt)
+    src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
+    tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+
+    enc_output = src_embedded
+    for enc_layer in self.encoder_layers:
+      enc_output = enc_layer(enc_output, src_mask)
+    
+    dec_output = tgt_embedded
+    for dec_layer in self.decoding_layers:
+      dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_mask)
+    
+    output = self.fully_connected_layer(dec_output)
+    return output
+    
 
