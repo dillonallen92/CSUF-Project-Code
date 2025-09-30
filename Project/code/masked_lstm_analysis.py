2a88ab0277c222574d7fc25d4985ea2af1f4c248
dillonallen92 <dillon.allen.92@gmail.com>
2025-09-30 13:56:48 -0700

    laying down some boilerplate before starting

--- Project/code/masked_lstm_analysis.py
+++ Project/code/masked_lstm_analysis.py
@@ -5,8 +5,18 @@ import torch.nn as nn
 import torch.optim as optim
 import pandas as pd 
 
+def read_data(file_path:str) -> pd.DataFrame:
+    data: pd.DataFrame = pd.read_csv(file_path)
+    return data
+
 
 
 
 if __name__ == "__main__":
-  print("Hello World")
+    
+    df_fresno_agg = read_data("Project/data/Fresno_Aggregate.csv")
+    best_window_vals = read_data("Project/data/fresno_lstm_best_feature_window_results.csv")
+    print(" --- Fresno Aggregate Data --- ")
+    print(df_fresno_agg)
+    print(" --- Best Window Values --- ")
+    print(best_window_vals)
